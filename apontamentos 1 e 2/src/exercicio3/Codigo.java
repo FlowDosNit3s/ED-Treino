7d50fbedc69bd533b9d18238330ffb42df54d1d6
FlowDosNit3s <diogocosta.dc3@gmail.com>
2026-09-23 20:49:01 +0100

    a

---
+++ apontamentos 1 e 2/src/exercicio3/Codigo.java
@@ -0,0 +1,32 @@
+package exercicio3;
+
+public class Codigo<T, U> {
+    private T t;
+    private U u;
+
+    public Codigo(T t, U u) {
+        this.t = t;
+        this.u = u;
+    }
+
+    public T getT() {
+        return this.t;
+    }
+
+    public U getU() {
+        return this.u;
+    }
+
+    public void setT(T t) {
+        this.t = t;
+    }
+
+    public void setU(U u) {
+        this.u = u;
+    }
+
+    @Override
+    public String toString() {
+        return "" + t + u;
+    }
+}
---
+++ apontamentos 1 e 2/src/exercicio3/ProdutoDemo.java
@@ -0,0 +1,21 @@
+package exercicio3;
+
+public class ProdutoDemo {
+    public static void main(String[] args) {
+        Codigo<String, Integer> c1 = new Codigo<>("TER", 1232);
+        Codigo<String, String> c2 = new Codigo<>("SEC", "1596");
+        Codigo<Integer, Integer> c3 = new Codigo<>(141, 414);
+
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+
+        ProdutoG2<Codigo<String, Integer>, Double> p1 = new ProdutoG2<>(c1, "Teclado Mecanico", 50.00);
+        ProdutoG2<Codigo<String, String>, Double> p2 = new ProdutoG2<>(c2, "Impressora", 120.0);
+        ProdutoG2<Codigo<Integer, Integer>, Float> p3 = new ProdutoG2<>(c3, "Monitor", 199.90f);
+
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
+    }
+}
